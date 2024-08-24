4a62a789fe0f2096c7438b95e53a29633158384c
Rokeeb Shobiye <icode.debug@gmail.com>
2024-08-24 17:52:45 +0100

    Today's lesson

---
+++ main.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+const os = require('os');
+const fs = require('fs')
+
+console.log("Hello world!")
+console.log("We are using node "+process.env.NODE_ENGINE+" for this class")
+console.log(`We are using node ${process.env.NODE_ENGINE} for this class`)
+
+console.log(os.totalmem())
+
+fs.readFile('attendance.txt', 'utf8', (err, data) => {
+    console.log(data)
+})
